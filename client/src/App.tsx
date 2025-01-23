cc3bfce5988fc30ece0965ecec247b38028ce3d5
Ricardo Simão <ricardo.simao.gmail.com>
2025-01-23 18:54:41 +0000

    Add vite

---
+++ client/src/App.tsx
@@ -0,0 +1,22 @@
+const products = [
+  {name: 'product1', price: 100.00},
+  {name: 'product2', price: 200.00},
+  {name: 'product2', price: 300.00},
+]
+
+function App() {
+
+
+  return (
+    <div>
+       <h1>SnowXtreme</h1>
+        <ul>
+          {products.map((item, index) => (
+            <li key={index}>{item.name} - {item.price}</li>
+          ))}
+        </ul> 
+    </div>
+  )
+}
+
+export default App
