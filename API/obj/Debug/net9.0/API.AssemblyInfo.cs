b05e518586de8c29f67deff33218ce21deaa9e2e
Ricardo Simão <ricardo.simao.gmail.com>
2025-01-26 19:18:13 +0000

    Adding the useEffecthook

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5b1146fc8f619b062f57c440390f7e703f0f1a4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc3bfce5988fc30ece0965ecec247b38028ce3d5")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
