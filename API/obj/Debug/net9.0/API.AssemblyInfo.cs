418b5858b391f43086722901fb1fe26ef0484b6c
Ricardo Simão <ricardo.simao.gmail.com>
2025-03-02 10:53:30 +0000

    Add ProductCard and ProductList Components

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47a08e8fb7638bb7add2e6df2bdc679c869c8d40")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2898647e4b63593b2c055c67f3501b243d6c2120")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
